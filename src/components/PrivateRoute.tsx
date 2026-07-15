3311724fd277fc16bccb78000e37c9b3c34d362d
FeruzDev <feruzjalilov7314@gmail.com>
2026-07-15 16:31:57 +0500

    sadaswwwd

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
-    const token = localStorage.getItem('tmk-token');
+    const token = localStorage.getItem('tmk-token-bgs');
 
     if (!token) {
         return <Navigate to="/" replace />;
